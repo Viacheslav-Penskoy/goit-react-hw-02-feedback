9ce486d17897bcc35904904cad12d5be85b31244
Viacheslav-Penskoy <114249044+Viacheslav-Penskoy@users.noreply.github.com>
2023-05-08 11:22:55 +0300

    Notification correct

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import css from './Notificstion.module.css';
+import css from './Notification.module.css';
 
 export const Notification = ({ message }) => {
   return message && <p className={css.p}>{message}</p>;
